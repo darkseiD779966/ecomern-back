ce69756579b6da6e5f8527a5b5db6dda6c45cc87
darkseiD779966 <ibrahim.darkseid@gmail.com>
2023-04-29 19:58:01 +0530

    first commit

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionStr = "Your mongoose connection string";
+const connectionStr = "mongodb+srv://ibrahimdarkseid:inayA2520@cluster0.e1n8pqg.mongodb.net/test";
 
 mongoose.connect(connectionStr, {useNewUrlparser: true})
 .then(() => console.log('connected to mongodb'))
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'http://localhost:3001',
+  cors: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
