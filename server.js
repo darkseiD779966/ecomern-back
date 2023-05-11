df479fed56e1264c7a4a869103ba0008ad24476d
darkseiD779966 <123389349+darkseiD779966@users.noreply.github.com>
2023-05-11 15:28:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const http = require('http');
+const https = require('https');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 require('./connection')
