880b0776e1606d07d07a69d818bb6c64a56b147b
darkseiD779966 <123389349+darkseiD779966@users.noreply.github.com>
2023-05-12 15:02:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,12 +22,7 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce7-w4hj.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/users', userRoutes);
